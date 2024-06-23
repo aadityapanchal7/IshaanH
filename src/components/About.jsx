a727b3621c4377bfa99b89da9790f23f15d7a016
wilder09 <aadityapanchal289@gmail.com>
2024-06-23 17:25:58 -0500

    fixed grammar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ function About() {
       </div>
       <div className='mt-5 space-y-5 text-lg font-thin leading-relaxed md:text-xl xl:text-2xl'>
         <p>
-          Hey there! I'm a {age} year currently in my senior year of high school. I'm all about blending the world of technology and business, especially when it comes to innovative solutions like blockchain. It's fascinating to see how these technologies can transform industries and redefine our future.
+          Hey there! I'm a {age}-year-old currently in my senior year of high school. I'm all about blending the world of technology and business, especially when it comes to innovative solutions like blockchain. It's fascinating to see how these technologies can transform industries and redefine our future.
         </p>
         <p>
           When I'm not deep in undertaking new projects, you can find me on the sand playing volleyball, or crushing it in a game of spikeball. I'm always up for a challenge, whether it's on my computer or on the court!
