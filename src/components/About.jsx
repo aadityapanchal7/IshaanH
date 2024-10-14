8f7c7c11409db090f66748cb822c8dccde6b480d
IshaanHemrajani <31942445+IshaanHemrajani@users.noreply.github.com>
2024-10-14 14:12:55 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,10 @@ function About() {
       </div>
       <div className='mt-5 space-y-5 text-lg font-thin leading-relaxed md:text-xl xl:text-2xl'>
         <p>
-          Hey there! I'm a {age}-year-old currently in my senior year of high school. I'm all about blending the world of technology and business, especially when it comes to innovative solutions like blockchain. It's fascinating to see how these technologies can transform industries and redefine our future.
+          I'm interested in the intersection of technology and business, particularly blockchain's various applications for accessibility. It's intriguing to explore how these technologies can impact industries and shape future developments.
         </p>
         <p>
-          When I'm not deep in undertaking new projects, you can find me on the sand playing volleyball, or crushing it in a game of spikeball. I'm always up for a challenge, whether it's on my computer or on the court!
+          When I'm not deep in undertaking new projects, you can find me on the pickleball court. I'm always up for a challenge, whether it's on my computer or on the court!
         </p>
       </div>
     </div>
