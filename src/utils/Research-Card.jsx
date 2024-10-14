a81de3102e1a89dc955b0c166bd1c3d2b29777fa
IshaanHemrajani <31942445+IshaanHemrajani@users.noreply.github.com>
2024-10-14 14:21:29 -0500

    Update Research-Card.jsx

--- src/utils/Research-Card.jsx
+++ src/utils/Research-Card.jsx
@@ -3,7 +3,7 @@ export const Research = [
     image: './assets/GMU.png',
     width: '80',
     alt: 'Logo',
-    Title: 'Finance',
+    Title: 'George Mason University',
     Role: 'Intern', 
     Date: '/ 2023-10-27',
     Description: "The Security of Arbitrum's optimistic roll-up implementation",
@@ -11,7 +11,7 @@ export const Research = [
   },
   {
     image: './assets/curiex.png',
-    Title: 'Finance',
+    Title: 'Independent Research',
     Date: '/ 2024-9-10',
     Description: "Blockchain in Cloud Computing              ",
     Link: "https://www.curieuxacademicjournal.com/_files/ugd/99711c_a8e5cd6b884e434f9cd883da0cd139b2.pdf#page=304"
