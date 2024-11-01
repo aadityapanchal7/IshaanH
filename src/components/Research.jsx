b72ec30db83612c81c057779399bc9029351931d
IshaanHemrajani <31942445+IshaanHemrajani@users.noreply.github.com>
2024-10-31 20:35:05 -0500

    Update Research.jsx

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -5,7 +5,7 @@ function Research() {
   return (
     <div className=' pt-14' id='Research'>
       <div className='sticky top-0 w-full py-2 text-xl font-bold text-green-700 md:text-2xl lg:text-3xl bg-white/100'>
-        //Research
+        Research
       </div>
       <div className='pt-5 '>
         <Card />
